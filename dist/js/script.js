66fdd7a1bf86ab71b624f96fb34e0ac4c5ff9917
EnTheos <theosenigma@gmail.com>
2022-08-09 16:10:57 +0300

    first page

---
+++ dist/js/script.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!**************************!*\
+  !*** ./src/js/script.js ***!
+  \**************************/
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  console.log(123);
+});
+/******/ })()
+;
+//# sourceMappingURL=script.js.map
---
+++ src/js/script.js
@@ -0,0 +1,5 @@
+"use strict";
+
+window.addEventListener('DOMContentLoaded', () => {
+    console.log(123);
+});
